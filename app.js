b9e8610a2ee3d22f447dab26a79012f4ccae93f8
iqpathscode <iqpaths.github@gmail.com>
2026-06-23 09:57:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
     credentials: true,
   }),
 );
-app.use(generalRateLimiter);
+// app.use(generalRateLimiter);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // <-- important for reading cookies
